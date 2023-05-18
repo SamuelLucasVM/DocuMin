2383fc9215057c58cc51debf5eb6ced49d9291e9
122110085 SAMUEL LUCAS VIEIRA MATOS <samuel.lucas.vieira.matos@lcc2-40.lcc.ufcg.edu.br>
2023-05-18 11:46:52 -0300

    feat: adding elementos package

--- src/elemento/Elemento.java
+++ src/elemento/Elemento.java
@@ -1,5 +1,18 @@
 package elemento;
 
-public class Elemento {
+import java.util.HashMap;
 
+public class Elemento {
+	private int prioridade;
+	private String valor;
+	private HashMap<String, String> propriedades;
+	
+	public String exibirCompleto() {
+		
+		return ;
+	}
+	
+	public String exibirResumido() {
+		
+	}
 }
